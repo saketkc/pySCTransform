d14af48c8c1bbceb132964df202e35b7b38cc3e6
Saket Choudhary <saketkc@gmail.com>
2022-08-08 20:59:20 -0400

    Bump version

--- pysctransform/__init__.py
+++ pysctransform/__init__.py
@@ -1,6 +1,6 @@
 """Top-level package for pySCTransform."""
 __author__ = """Saket Choudhary"""
 __email__ = "saketkc@gmail.com"
-__version__ = "0.1.1"
+__version__ = "0.1.1.dev0"
 
 from .pysctransform import get_hvg_residuals, vst, SCTransform
